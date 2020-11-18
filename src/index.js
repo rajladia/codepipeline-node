5b8194e1863ad21f2378db9a8f399549009653e2
geekgeekgo <66518176+geekgeekgo@users.noreply.github.com>
2020-11-18 16:51:05 +0530

    Delete index.js

--- src/index.js
+++
@@ -1,8 +0,0 @@
-const awsServerless=require('aws-serverless-express');
-const app=require('./app')
-const server=awsServerless.createServer(app)
-
-exports.handler=(event,context)=>{
-    console.log("event: "+ JSON.stringify(event));
-    awsServerless.proxy(server,event,context)
-}
